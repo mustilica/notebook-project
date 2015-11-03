d1a248821a338a0a1c65bdea210ef7e36541f505
mustilica <ilhan.mstf@gmail.com>
2015-11-03 07:45:24 +0300

    vertical align, navigation animation, seperator

--- js/agency.js
+++ js/agency.js
@@ -4,6 +4,15 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+ // jQuery to collapse the navbar on scroll
+ $(window).scroll(function() {
+     if ($(".navbar").offset().top > 50) {
+         $(".navbar-fixed-top").addClass("navbar-shrink");
+     } else {
+         $(".navbar-fixed-top").removeClass("navbar-shrink");
+     }
+ });
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
